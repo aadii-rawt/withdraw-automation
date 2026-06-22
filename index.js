3d7395bd35a9ec0a4871d7dbef3a6adc07da0bbc
aadii-rawt <rawatadii060@gmail.com>
2026-06-22 15:25:46 +0530

    track  current row

--- index.js
+++ index.js
@@ -17,28 +17,34 @@ const { chromium } = require("playwright");
 
     await websiteA.bringToFront();
 
-    await websiteA.locator(".details-notes-cmn-img").first().click();
+    await websiteA.locator(".details-notes-cmn-img").nth(1).click();
+    // const rows = websiteA.locator(".details-notes-cmn-img");
 
-    // const accountNumber = await websiteA.locator(
-    //     'p:text("Account Number :-")'
-    // ).locator('xpath=following-sibling::p[1]')
-    //     .textContent();
-    // const ifsc = await websiteA.locator(
-    //     'p:text("IFSC")'
-    // ).locator('xpath=following-sibling::p[1]')
-    //     .textContent();
-    // const name = await websiteA.locator(
-    //     'p:text("Holder Name :-")'
-    // ).locator('xpath=following-sibling::p[1]')
-    //     .textContent();
-    // const amount = await websiteA.locator(
-    //     'p:text("Amount :-")'
-    // ).locator('xpath=following-sibling::p[1]')
-    //     .textContent();
-    const accountNumber = "5750838958";
-    const ifsc = "KKBK0000173";
-    const name = "Aditya Rawat";
-    const amount = "1";
+    // const count = await rows.count();
+    // console.log("row count ", count);
+    
+
+
+    const accountNumber = await websiteA.locator(
+        'p:text("Account Number :-")'
+    ).locator('xpath=following-sibling::p[1]')
+        .textContent();
+    const ifsc = await websiteA.locator(
+        'p:text("IFSC")'
+    ).locator('xpath=following-sibling::p[1]')
+        .textContent();
+    const name = await websiteA.locator(
+        'p:text("Holder Name :-")'
+    ).locator('xpath=following-sibling::p[1]')
+        .textContent();
+    const amount = await websiteA.locator(
+        'p:text("Amount :-")'
+    ).locator('xpath=following-sibling::p[1]')
+        .textContent();
+    // const accountNumber = "5750838958";
+    // const ifsc = "KKBK0000173";
+    // const name = "Aditya Rawat";
+    // const amount = "1";
 
     console.log(accountNumber, ifsc, name, amount);
     // switch to k1 
@@ -149,7 +155,7 @@ const { chromium } = require("playwright");
     await websiteA.locator(".btn-close").first().click()
 
     await websiteA.getByTitle("Approve").first().click()
-      await websiteB.waitForTimeout(3000);
+    await websiteB.waitForTimeout(3000);
 
 
     const utrInput = websiteB.locator(
